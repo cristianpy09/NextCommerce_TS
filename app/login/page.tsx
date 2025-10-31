db6b4992d1b52a44095654b478ba067bed416bbe
sebastianSPM <sebasperemald04@gmail.com>
2025-10-31 11:49:20 -0500

    created login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import order from '@/public/images/order_14379893.png'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-[#264ECA] relative overflow-hidden">
+      {/* Fondo con ondas suaves */}
+      <div className="absolute inset-0 bg-gradient-to-br from-[#1A2980] to-[#26D0CE] opacity-90"></div>
+
+
+
+      {/* Contenedor principal */}
+      <div className="relative z-10 flex flex-col items-center justify-center bg-gradient-to-br from-[#182848] to-[#4CA1AF] p-6 rounded-2xl text-white shadow-2xl backdrop-blur-md h-[400px] w-[350px] border border-white/20">
+
+        
+        {/* Ícono de carrito */}
+        <div className="mb-6">
+          <Image
+             src={order}
+             alt="Shopping cart"
+             width={100}
+             height={100}
+             className="rounded-full"
+          
+          />
+        </div>
+
+        {/* Formulario */}
+        <div className="flex flex-col space-y-4 w-full">
+          <label>USERNAME:</label>
+          <div className="border border-white/60 rounded-md px-4 py-2 flex items-center">
+            <input
+              type="text"
+              placeholder="USERNAME"
+              className="bg-transparent outline-none text-sm text-white placeholder-white/70 w-full"
+            />
+          </div>
+
+          <label>PASSWORD:</label>
+          <div className="border border-white/60 rounded-md px-4 py-2 flex items-center">
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              className="bg-transparent outline-none text-sm text-white placeholder-white/70 w-full"
+            />
+          </div>
+
+          <button className="bg-white text-[#264ECA] font-semibold mt-5 py-2 cursor-pointer rounded-md shadow hover:bg-gray-100 transition-all">
+            LOGIN
+          </button>
+        </div>
+
+        {/* Enlace inferior */}
+        <a
+          href="#"
+          className="text-sm text-white/80 mt-30 hover:text-white transition-all"
+        >
+          Forgot password?
+        </a>
+      </div>
+    </div>
+  );
 }
