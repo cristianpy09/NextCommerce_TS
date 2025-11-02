b89bc7ac31872b826ed78da5450c0c309ae261ef
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-02 14:57:54 -0500

    search bar added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ProductList from '../components/ProductList'
+import React from "react";
+import ProductList from "../components/ProductList";
+import FilterBar from "../components/FilterBar";
+import SearchBar from "../components/SearchBar";
 
 export default function Dashboard() {
   return (
-    <section className='mt-16'>
-        <ProductList/>
+    <section className="mt-16">
+      <div className=" ml-6 mt-4 flex space-x-16">
+        <FilterBar />
+        <SearchBar />
+      </div>
+
+      <ProductList />
     </section>
-  )
+  );
 }
