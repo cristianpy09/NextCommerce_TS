bf63ceea06a953fcc039a40530866deaf181942c
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-11 09:15:19 -0500

    feat:authentication added

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,12 +1,14 @@
+import { Router } from "next/router";
 import { users } from "../data/users";
 import { User } from "../types/usersType";
+import { redirect } from "next/navigation";
 
 export function auth(email: string) {
   const data = users;
 
-  const user = data.find((u) => user?.email === email);
+  const user = data.find((u) => u?.email === email);
 
   if (user) {
-    return console.log("encontrado");
+    return redirect("/")
   }
 }
