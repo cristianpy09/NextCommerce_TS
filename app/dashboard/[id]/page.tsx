3aafca87930079a3f03e9b7f6c32a300a8320c98
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-11 18:00:13 -0500

    nothing

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,7 +1,7 @@
-import ProductCard from '@/app/components/ProductCard';
-import { products } from '@/app/data/products';
-import { Product } from '@/app/types/productsType';
-import React from 'react'
+import ProductCard from "@/app/components/ProductCard";
+import { products } from "@/app/data/products";
+import { Product } from "@/app/types/productsType";
+import React from "react";
 
 interface Props {
   params: {
@@ -9,23 +9,22 @@ interface Props {
   };
 }
 
-export default async function ProductDetailpage({params}:Props) {
-      const data = products
-      const {id}= await params
-     const producto = data.find((p) => p.sku === id);
+export default async function ProductDetailpage({ params }: Props) {
+  const data = products;
+  const { id } = await params;
+  const producto = data.find((p) => p.sku === id);
 
-   
-    
- 
-
- if (producto) {
-    
- return (
-    <div>
-
-        <ProductCard name={producto.name} category={producto.category} description={producto.description} price={producto.price} img={producto.imageUrl} />
-    </div>
-
-  ) } 
-
+  if (producto) {
+    return (
+      <div>
+        <ProductCard
+          name={producto.name}
+          category={producto.category}
+          description={producto.description}
+          price={producto.price}
+          img={producto.imageUrl}
+        />
+      </div>
+    );
+  }
 }
