f11879ed3f5f4f69be5eb873ed62b9dbcba6870d
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-20 07:47:18 -0500

    search bar with filters finished

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,76 +2,94 @@
 
 import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../context/CartContext";
+import { products } from "../data/products";
 
 export default function SearchBar() {
-  const [text, setText] = useState("");
-  const [debounced, setDebounced] = useState("");
-  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
   const Products = useContext(CartContext);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setDebounced(text);
-    }, 900);
-    return () => clearTimeout(timer);
-  }, [text]);
 
-  useEffect(() => {
-    console.log("debounced:", debounced);
-  }, [debounced]);
+  const handleSearch = (e: any) => {
+    const value = e.target.value;
+    setSearch(value);
 
-  const clickBusqueda = (e:any)=>{
-    if (debounced==="Calzado") {
-      console.log("solo mostrar calzado");
-       
-        if (Products) {
-          const filterProducts=Products.productosfinales.filter(product=>(product.category==="Calzado"))
-          const setProductos = Products.setProductosfinales
-          setProductos(filterProducts)
+    if (!Products) return;
+
+    if (value.length >= 2) {
+      const filtered = products.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      Products.setProductosfinales(filtered);
+    } else {
+      Products.setProductosfinales(products);
     }
-    setOpen(false)
-  }  }
+  };
+
+  const clickBusquedaCategoria = (categoria: string) => {
+    if (!Products) return;
+
+    const filtered = products.filter(
+      (item) => item.category.toLowerCase() === categoria.toLowerCase()
+    );
+
+    Products.setProductosfinales(filtered);
+  };
+
   return (
-    <>
-      <div className="dropdown w-full">
-        {/* INPUT */}
-        <label className="input w-full flex items-center gap-2">
-          <svg
-            className="h-[1em] opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+    <div className="dropdown w-full">
+      <label className="input w-full flex items-center gap-2">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
           >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </g>
-          </svg>
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
 
-          <input
-            onClick={() => setOpen(true)}
-            onChange={(e) => setText(e.target.value)}
-            type="search"
-            className="grow"
-            placeholder="Search"
-          />
-        </label>
+        <input
+          type="text"
+          className="grow"
+          placeholder="Search"
+          value={search}
+          onChange={handleSearch}
+        />
+      </label>
 
-        {open && (
-          <ul className="dropdown-content menu bg-base-100 rounded-box w-208 p-2 shadow z-[100] mt-0">
-            <li>
-              <button
-                onClick={clickBusqueda}
-              >
-                {debounced}
-              </button>
-            </li>
-          </ul>
-        )}
-      </div>
-    </>
-)}
+      <ul className="dropdown-content menu bg-base-100 rounded-b-sm w-full p-2 shadow border">
+        <li>
+          <h1 className="font-bold">Lo mas buscado</h1>
+        </li>
+        <li>
+          <button onClick={() => clickBusquedaCategoria("Calzado")}>
+            Calzado
+          </button>
+        </li>
+        <li>
+          <button onClick={() => clickBusquedaCategoria("Tecnologia")}>
+            Tecnología
+          </button>
+        </li>
+        <li>
+          <button onClick={() => clickBusquedaCategoria("Moda")}>Moda</button>
+        </li>
+        <li>
+          <button onClick={() => clickBusquedaCategoria("Hogar")}>Hogar</button>
+        </li>
+        <li>
+          <button onClick={() => clickBusquedaCategoria("Accesorios")}>
+            Accesorios
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
